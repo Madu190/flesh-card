327f31c33a742277b98f940dca83f1deb8b7a94b
Madu190 <161855374+Madu190@users.noreply.github.com>
2024-11-29 12:05:37 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,61 @@ criaCartao(
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
+criaCartao(
+    'Esporte',
+    ' Qual país sediou os primeiros Jogos Olímpicos modernos em 1896?',
+    'Grécia'
+)
+
+criaCartao(
+    'Esporte',
+    ' Quem é considerado o maior artilheiro da história da Copa do Mundo de Futebol?',
+     Miroslav Klose''
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Qual é o tempo verbal usado para expressar uma ação que aconteceu no passado e continua no presente?',
+    'Present Perfect'
+)
+criaCartao(
+    'Programação',
+    'Qual das opções é uma linguagem de programação orientada a objetos?',
+    ' Python'
+)
+criaCartao(
+    'musica',
+    'Quem é o autor da famosa composição clássica "Uma Noite na Floresta"?',
+    'Johann Strauss II'
+)
+criaCartao(
+    'muica',
+    'Qual banda lançou o álbum "Abbey Road" em 1969?',
+    ' The Beatles'
+)
+criaCartao(
+    'historia',
+    ' Quem foi o primeiro imperador de Roma?',
+    'César Augusto'
+)
+criaCartao(
+    'historia',
+    'Em que ano ocorreu a queda do Muro de Berlim?',
+    ' 1989'
+)
+criaCartao(
+    'historia',
+    'Qual foi a principal causa da Primeira Guerra Mundial?',
+    'A assassinação do arquiduque Francisco Ferdinando '
+)
